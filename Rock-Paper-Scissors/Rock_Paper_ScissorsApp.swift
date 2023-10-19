709c90b31ecc0d31dbacd91d7f248812527e93a6
Dillon Teakell <teakelld@icloud.com>
2023-10-19 12:26:08 -0400

    Initial Commit

---
+++ Rock-Paper-Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock-Paper-Scissors
+//
+//  Created by Dillon Teakell on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
